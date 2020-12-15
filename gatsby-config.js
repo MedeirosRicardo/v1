c49a03bf369b77befa1cb0ffac89b6e965af0337
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-12-14 21:34:21 -0500

    Basic pages created

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-material-ui",
     {
       resolve: "gatsby-source-filesystem",
       options: {
---
+++ src/pages/portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PortfolioPage = () => {
+  return (
+    <React.Fragment>
+      <p>Portfolio page!!!</p>
+    </React.Fragment>
+  );
+}
+
+export default PortfolioPage;
